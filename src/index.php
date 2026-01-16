bf285bbe4052cfce619fd03419779a226d4dc6f2
W. Latif Ayubi <wayubi@gmail.com>
2026-01-15 19:57:18 -0800

    refactored css

--- src/index.php
+++ src/index.php
@@ -235,7 +235,7 @@ foreach ($audioThumbsRaw as $audioFs => $thumbFs) {
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Xclusive Media Player</title>
-        <link rel="stylesheet" href="/assets/css/app.min.css">
+        <link rel="stylesheet" href="/assets/css/app.css">
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet">
